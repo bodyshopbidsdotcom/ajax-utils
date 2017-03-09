f292625cfe11c75b003335967d2c651228c2a400
Nick Stanish <nickstanish@gmail.com>
2017-03-09 13:09:47 -0600

    add field for additional data in ApiError

--- src/ApiError.js
+++ src/ApiError.js
@@ -1,9 +1,10 @@
 import ExtendableError from 'extendable-error';
 
 export default class ApiError extends ExtendableError {
-  constructor(message, statusCode, response) {
+  constructor(message, statusCode, response, additionalData) {
     super(message);
     this.statusCode = statusCode;
     this.response = response;
+    this.data = additionalData;
   }
 }
